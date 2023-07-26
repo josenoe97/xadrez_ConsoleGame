c814f1bd782a480693d3d815dac1d2c7da12c6b3
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-07-26 09:13:52 -0300

    Tratando erros com excecao personalizada

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -9,15 +9,23 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-            Tabuleiro tabuleiro = new Tabuleiro(8 , 8);
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
+                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 9));
 
-            tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-            tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-            tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
 
-            Tela.imprimirTabuleiro(tabuleiro);
 
+                Tela.imprimirTabuleiro(tabuleiro);
+            }
+            catch (TabuleiroException ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.WriteLine();
         }
     }
---
+++ xadrez_console/tabuleiro/Exceptions/TabuleiroException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+     class TabuleiroException : Exception
+    {
+        public TabuleiroException(string menssagem) 
+            : base(menssagem)
+        {
+        }
+    }
+}
